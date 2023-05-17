b455773c7c92d91830c6379de732d6a0c3f42510
Alvyn <>
2023-05-17 07:31:12 +0200

    Setup V1 exercice project

---
+++ V1Exercice/V1ExerciceApp.swift
@@ -0,0 +1,15 @@
+//
+//  V1ExerciceApp.swift
+//  V1Exercice
+//
+
+import SwiftUI
+
+@main
+struct V1ExerciceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
